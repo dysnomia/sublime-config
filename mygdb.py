01002bf8f11d06e893616a752031c9d04be0f7d7
Daniel Hoske <daniel.hoske@gmail.com>
2013-06-06 19:55:05 +0200

    My GDB plugin functional.

--- mygdb.py
+++ mygdb.py
@@ -1,18 +1,19 @@
-import sublime, sublime_plugin
+import sublime_plugin
+
 
 class MyGdbCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		self.window.run_command('set_layout', {
-			"cols":[0.0, 0.5, 1.0],
-			"rows":[0.0, 0.8, 1.0],
-			"cells":[[0, 0, 1, 1], [1, 0, 2, 1], [0, 1, 2, 2]]
-		})
-		self.window.run_command('repl_open', {
-			"type": "subprocess",
-			"encoding": "utf8",
-			"cmd": ["gdb", "-i", "-u", "$file_path/a.out"],
-			"cwd": "$file_path",
-			#"syntax": "Packages/Python/Python.tmLanguage",
-			"external_id": "gdb"
-		})
-		self.window.run_command('move_to_group', { "group": 2 })
+    def run(self):
+        self.window.run_command('set_layout', {
+            "cols": [0.0, 0.5, 1.0],
+            "rows": [0.0, 0.8, 1.0],
+            "cells": [[0, 0, 1, 1], [1, 0, 2, 1], [0, 1, 2, 2]]
+        })
+        self.window.run_command('repl_open', {
+            "type": "subprocess",
+            "encoding": "utf8",
+            "cmd": ["gdb", "$file_path/a.out"],
+            "cwd": "$file_path",
+            "syntax": "Packages/C++/C++.tmLanguage",
+            "external_id": "gdb"
+        })
+        self.window.run_command('move_to_group', {"group": 2})
