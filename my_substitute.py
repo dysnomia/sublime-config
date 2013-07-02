58f91e55156a5a48151c6ef0644ef0a73400e1a8
Daniel Hoske <daniel.hoske@gmail.com>
2013-07-02 20:56:05 +0200

    Removed unused files.

--- my_substitute.py
+++ my_substitute.py
@@ -1,8 +1,8 @@
 import sublime_plugin
 
 
-# get selection and replace the strings in from with
-# the strings in to
+# get selection and replace the strings in 'from' with
+# the strings in 'to'
 class MySubstituteCommand(sublime_plugin.TextCommand):
     def run(self, edit, **args):
         for region in self.view.sel():
